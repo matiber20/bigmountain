42f8d13e1fc738e61e994783db5e31818cf02216
matiber20 <matiber20@gmail.com>
2021-04-19 13:03:04 -0300

    Creacion boton onAdd

---
+++ src/components/ItemCount/Counter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Counter({decrement,increment,carrito}) {
+    return (
+        <div>
+            <button onClick={increment}>+</button>
+            <button onClick={decrement}>-</button>
+            <button onClick={carrito}>Agregar al carrito</button>
+        </div>
+    )
+}
--- src/containers/CounterContainer/CounterContainer.js
+++ src/containers/CounterContainer/CounterContainer.js
@@ -1,27 +1,40 @@
 import React, { Component } from 'react'
-import Counter from '../../components/Counter/Counter'
+import Counter from '../../components/ItemCount/Counter'
 
 export default class CounterContainer extends Component {
 
     constructor(props){
         super(props)
         this.state = {
-            stock:10
+            stock:10,
+            carrito:0
         }
 
         this.increment= this.increment.bind(this)
         this.decrement= this.decrement.bind(this)
+        this.onAdd = this.onAdd.bind(this)
     }
 
     increment(){
-        this.setState({stock: this.state.stock+1})
+        if(this.state.stock > 0){
+            this.setState({stock: this.state.stock-1,
+                carrito: this.state.carrito+1})
+        }
+        
     }
 
     decrement(){
-        if(this.state.stock > 0){
-            this.setState({stock: this.state.stock-1})
+        if(this.state.carrito > 0){
+            this.setState({stock: this.state.stock+1,
+                        carrito: this.state.carrito-1})
         }
         
+    }
+
+    onAdd(){
+        if(this.state.carrito !== 0){
+            console.log(this.state.carrito);
+        }
     }
 
     componentDidMount(){
@@ -31,12 +44,15 @@ export default class CounterContainer extends Component {
     componentDidUpdate(){
         console.log('Se actualizo');
     }
+
+
     render() {
         return (
             <div>
-                <h3>El numero es: { this.state.stock}</h3>
+                <h1>En el carrito: {this.state.carrito}</h1>
+                <h3>Disponible: { this.state.stock}</h3>
                 {
-                    this.state.stock === 10 ? <Counter decrement={this.decrement} /> : <Counter decrement={this.decrement} increment={this.increment} />
+                    this.state.stock === 0 && this.state.stock === 11 ? null : <Counter decrement={this.decrement} increment={this.increment} carrito={this.onAdd} />
                 }
             </div>
         )
