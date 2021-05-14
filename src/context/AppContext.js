81af1ee2bf6cd23f404eff8e064c20b7c156dc83
matiber20 <matiber20@gmail.com>
2021-05-14 12:13:07 -0300

    creacion state inCart

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -29,7 +29,7 @@ export default function AppContextProvider({children}) {
     function onAdd(item){
         if(number !== 0){
             setCart(true)
-            setInCart(item)            
+            setInCart(...inCart, item)            
         }else{
             setCart(false)
         }
